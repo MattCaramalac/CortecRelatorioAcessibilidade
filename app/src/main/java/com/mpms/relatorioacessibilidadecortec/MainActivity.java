c6e9142d73eca5341489f31980988e2bc2df9674
MattCaramalac <mattcaramalac@gmail.com>
2021-04-09 20:26:28 -0400

    Data input corrigida (ainda usando UTC)

--- app/src/main/java/com/mpms/relatorioacessibilidadecortec/MainActivity.java
+++ app/src/main/java/com/mpms/relatorioacessibilidadecortec/MainActivity.java
@@ -1,28 +1,28 @@
 package com.mpms.relatorioacessibilidadecortec;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mpms.relatorioacessibilidadecortec.model.ViewModelEntry;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewModelEntry viewModelEntry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       viewModelEntry = new ViewModelProvider.AndroidViewModelFactory(MainActivity.this.getApplication()).create(ViewModelEntry.class);
+
         FloatingActionButton fab = findViewById(R.id.fab_register);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
-            }
-        });
+        fab.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, RegisterActivity.class)));
 
 
     }
--- app/src/main/java/com/mpms/relatorioacessibilidadecortec/RegisterActivity.java
+++ app/src/main/java/com/mpms/relatorioacessibilidadecortec/RegisterActivity.java
@@ -4,15 +4,18 @@ import android.app.DatePickerDialog;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.DatePicker;
 import java.time.*;
+import java.time.zone.ZoneRules;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.datepicker.MaterialDatePicker;
-import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -21,25 +24,25 @@ public class RegisterActivity extends AppCompatActivity implements DatePickerDia
     public LocalDate chosenDate = null;
     public String chosenDateOldVersion = null;
 
-    TextInputEditText dateInspectionText = findViewById(R.id.date_inspection_value);
-    TextInputEditText nameSchool = findViewById(R.id.name_school_text);
-    TextInputEditText nameCity = findViewById(R.id.name_city_text);
-    TextInputEditText nameResponsible = findViewById(R.id.name_responsible_text);
-    TextInputEditText totalStudents = findViewById(R.id.total_students_text);
-    TextInputEditText totalStudentsPcd = findViewById(R.id.students_pcd_text);
-    TextInputEditText totalWorkers = findViewById(R.id.total_workers_text);
-    TextInputEditText totalWorkersPcd = findViewById(R.id.workers_pcd_text);
-    TextInputEditText totalWorkersLibras = findViewById(R.id.workers_libras_text);
+    TextInputEditText dateInspectionText;
+    TextInputEditText nameSchool;
+    TextInputEditText nameCity;
+    TextInputEditText nameResponsible;
+    TextInputEditText totalStudents;
+    TextInputEditText totalStudentsPcd;
+    TextInputEditText totalWorkers;
+    TextInputEditText totalWorkersPcd;
+    TextInputEditText totalWorkersLibras;
 
-    TextInputLayout schoolField = findViewById(R.id.name_school);
-    TextInputLayout cityField = findViewById(R.id.name_city_school);
-    TextInputLayout responsibleField = findViewById(R.id.name_responsible);
-    TextInputLayout totStudentsField = findViewById(R.id.total_students);
-    TextInputLayout totStudentsPcd = findViewById(R.id.total_students_pcd);
-    TextInputLayout totWorkersField = findViewById(R.id.total_workers);
-    TextInputLayout totWorkersPcd = findViewById(R.id.total_workers_pcd);
-    TextInputLayout totWorkersLibras = findViewById(R.id.total_workers_libras);
-    TextInputLayout dateField = findViewById(R.id.date_inspection);
+    TextInputLayout schoolField;
+    TextInputLayout cityField;
+    TextInputLayout responsibleField;
+    TextInputLayout totStudentsField;
+    TextInputLayout totStudentsPcd;
+    TextInputLayout totWorkersField;
+    TextInputLayout totWorkersPcd;
+    TextInputLayout totWorkersLibras;
+    TextInputLayout dateField;
 
     Button saveButton;
 
@@ -47,6 +50,29 @@ public class RegisterActivity extends AppCompatActivity implements DatePickerDia
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_school_register);
+
+        //Só podem ser iniciados DENTRO do onCreate, caso contrário não foi selecionada ainda a View
+        //E acaba causando um apontamento para algo nulo (já que não tem View selecioada para ser achada
+
+        dateInspectionText = findViewById(R.id.date_inspection_value);
+        nameSchool = findViewById(R.id.name_school_text);
+        nameCity = findViewById(R.id.name_city_text);
+        nameResponsible = findViewById(R.id.name_responsible_text);
+        totalStudents = findViewById(R.id.total_students_text);
+        totalStudentsPcd = findViewById(R.id.students_pcd_text);
+        totalWorkers = findViewById(R.id.total_workers_text);
+        totalWorkersPcd = findViewById(R.id.workers_pcd_text);
+        totalWorkersLibras = findViewById(R.id.workers_libras_text);
+
+        schoolField = findViewById(R.id.name_school);
+        cityField = findViewById(R.id.name_city_school);
+        responsibleField = findViewById(R.id.name_responsible);
+        totStudentsField = findViewById(R.id.total_students);
+        totStudentsPcd = findViewById(R.id.total_students_pcd);
+        totWorkersField = findViewById(R.id.total_workers);
+        totWorkersPcd = findViewById(R.id.total_workers_pcd);
+        totWorkersLibras = findViewById(R.id.total_workers_libras);
+        dateField = findViewById(R.id.date_inspection);
 
         saveButton = findViewById(R.id.saveButton);
 
@@ -65,14 +91,14 @@ public class RegisterActivity extends AppCompatActivity implements DatePickerDia
                 .setSelection(MaterialDatePicker.todayInUtcMilliseconds()).build();
         datePickerDialog.show(getSupportFragmentManager(), "DATE_PICKER");
 
-        datePickerDialog.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
-            @Override
-            public void onPositiveButtonClick(Long selection) {
-                dateInspectionText.setText(dateToString(selection));
-            }
-        });
-//        datePickerDialog.addOnPositiveButtonClickListener((MaterialPickerOnPositiveButtonClickListener)
-//                selection -> chosenDate.setText(datePickerDialog.getHeaderText()));
+        datePickerDialog.addOnPositiveButtonClickListener(selection -> dateInspectionText.setText(dateToString(selection)));
+//          Sem expressão lambda fica assim:
+//        datePickerDialog.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
+//            @Override
+//            public void onPositiveButtonClick(Long selection) {
+//             dateInspectionText.setText(dateToString(selection));
+//            }
+//        });
     }
 
     @Override
@@ -127,13 +153,14 @@ public class RegisterActivity extends AppCompatActivity implements DatePickerDia
 
     public String dateToString(Long dateLong) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            chosenDate = Instant.ofEpochMilli(dateLong).atZone(ZoneId.systemDefault()).toLocalDate();
-            return chosenDate.getDayOfMonth() +"/"+ chosenDate.getMonthValue() +"/"+ chosenDate.getYear();
+//            systemDefault()
+            chosenDate = Instant.ofEpochMilli(dateLong).atZone(ZoneId.of("Z")).toLocalDate();
+            return lessThanTen(chosenDate.getDayOfMonth()) +"/"+ lessThanTen(chosenDate.getMonthValue()) +"/"+ chosenDate.getYear();
         } else {
             Calendar calendar = Calendar.getInstance();
             calendar.setTimeInMillis(dateLong);
             chosenDateOldVersion = calendar.YEAR +"-"+ lessThanTen((calendar.MONTH + 1))  +"-"+ lessThanTen(calendar.DAY_OF_MONTH);
-            return calendar.DAY_OF_MONTH + "/" + calendar.MONTH + 1 + "/" + calendar.YEAR;
+            return lessThanTen(calendar.DAY_OF_MONTH) + "/" + lessThanTen((calendar.MONTH + 1)) + "/" + calendar.YEAR;
         }
     }
 
@@ -143,4 +170,6 @@ public class RegisterActivity extends AppCompatActivity implements DatePickerDia
         else
             return Integer.toString(number);
     }
+
+
 }
