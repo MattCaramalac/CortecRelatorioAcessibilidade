0bb84725ba16513bbbb63087d54a7cef04482c04
MattCaramalac <mattcaramalac@gmail.com>
2021-04-11 18:17:36 -0400

    Alteração de ListView para RecyclerView

--- app/src/main/java/com/mpms/relatorioacessibilidadecortec/activities/MainActivity.java
+++ app/src/main/java/com/mpms/relatorioacessibilidadecortec/activities/MainActivity.java
@@ -1,17 +1,18 @@
-package com.mpms.relatorioacessibilidadecortec;
+package com.mpms.relatorioacessibilidadecortec.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mpms.relatorioacessibilidadecortec.R;
 import com.mpms.relatorioacessibilidadecortec.model.ViewModelEntry;
 
 public class MainActivity extends AppCompatActivity {
 
+//    private static final int MAIN_ACTIVITY_REQ_CODE = 1;
     private ViewModelEntry viewModelEntry;
 
     @Override
@@ -21,8 +22,29 @@ public class MainActivity extends AppCompatActivity {
 
        viewModelEntry = new ViewModelProvider.AndroidViewModelFactory(MainActivity.this.getApplication()).create(ViewModelEntry.class);
 
+       viewModelEntry.getAllEntries().observe(this, schoolEntries -> {
+
+       });
+
+//        contactViewModel.getAllContacts().observe(this, contacts -> {
+//
+//            recyclerViewAdapter = new RecyclerViewAdapter(contacts, MainActivity.this, this);
+//            recyclerView.setAdapter(recyclerViewAdapter);
+//
+//        });
+
         FloatingActionButton fab = findViewById(R.id.fab_register);
         fab.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, RegisterActivity.class)));
+
+//        {
+//            startActivityForResult(new Intent(MainActivity.this, RegisterActivity.class), MAIN_ACTIVITY_REQ_CODE);
+//                });
+
+
+
+
+
+
 
 
     }
