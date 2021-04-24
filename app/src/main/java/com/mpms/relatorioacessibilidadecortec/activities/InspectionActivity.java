c0f2958afdf239e96439608bf6fd8d5badf769c5
MattCaramalac <mattcaramalac@gmail.com>
2021-04-23 20:55:28 -0400

    Adição Mais Fragments Memorial

--- app/src/main/java/com/mpms/relatorioacessibilidadecortec/activities/InspectionActivity.java
+++ app/src/main/java/com/mpms/relatorioacessibilidadecortec/activities/InspectionActivity.java
@@ -5,12 +5,13 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
+import com.mpms.relatorioacessibilidadecortec.fragments.CoordinationFragment;
 import com.mpms.relatorioacessibilidadecortec.fragments.ExternalAccessFragment;
 import com.mpms.relatorioacessibilidadecortec.R;
 import com.mpms.relatorioacessibilidadecortec.fragments.InspectionMemorial;
+import com.mpms.relatorioacessibilidadecortec.fragments.LibraryFragment;
+import com.mpms.relatorioacessibilidadecortec.fragments.SidewalkFragment;
 import com.mpms.relatorioacessibilidadecortec.fragments.WaterFountainFragment;
 
 public class InspectionActivity extends AppCompatActivity implements InspectionMemorial.OnFragmentInteractionListener {
@@ -35,6 +36,15 @@ public class InspectionActivity extends AppCompatActivity implements InspectionM
                 break;
             case 1:
                 displayWaterFountainFragment();
+                break;
+            case 2:
+                displayLibraryFragment();
+                break;
+            case 3:
+                displaySidewalkFragment();
+                break;
+            case 4:
+                displayCoordinationFragment();
                 break;
         }
 
@@ -65,6 +75,48 @@ public class InspectionActivity extends AppCompatActivity implements InspectionM
         if (waterFountainFragment != null) {
             fragmentTransaction = fragmentManager.beginTransaction();
             fragmentTransaction.remove(waterFountainFragment).commit();
+        }
+    }
+
+    public void displayLibraryFragment() {
+        LibraryFragment libraryFragment = LibraryFragment.newInstance();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.show_fragment_selected,libraryFragment).addToBackStack(null).commit();
+    }
+
+    public void closeLibraryFragment() {
+        LibraryFragment libraryFragment = (LibraryFragment) fragmentManager.findFragmentById(R.id.show_fragment_selected);
+        if (libraryFragment != null) {
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(libraryFragment).commit();
+        }
+    }
+
+    public void displaySidewalkFragment() {
+        SidewalkFragment sidewalkFragment = SidewalkFragment.newInstance();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.show_fragment_selected,sidewalkFragment).addToBackStack(null).commit();
+    }
+
+    public void closeSidewalkFragment() {
+        SidewalkFragment sidewalkFragment = (SidewalkFragment) fragmentManager.findFragmentById(R.id.show_fragment_selected);
+        if (sidewalkFragment != null) {
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(sidewalkFragment).commit();
+        }
+    }
+
+    public void displayCoordinationFragment() {
+        CoordinationFragment coordinationFragment = CoordinationFragment.newInstance();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.show_fragment_selected,coordinationFragment).addToBackStack(null).commit();
+    }
+
+    public void closeCoordinationFragment() {
+        CoordinationFragment coordinationFragment = (CoordinationFragment) fragmentManager.findFragmentById(R.id.show_fragment_selected);
+        if (coordinationFragment != null) {
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(coordinationFragment).commit();
         }
     }
 }
