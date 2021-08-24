d37a55b3f1f3341d502c3e1a15e992e56d5ab1f1
MattCaramalac <mattcaramalac@gmail.com>
2021-08-24 17:09:02 -0400

    Início layout cadastro sanitários

--- app/src/main/java/com/mpms/relatorioacessibilidadecortec/fragments/RestroomFragment.java
+++ app/src/main/java/com/mpms/relatorioacessibilidadecortec/fragments/RestroomFragment.java
@@ -1,16 +1,25 @@
 package com.mpms.relatorioacessibilidadecortec.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.mpms.relatorioacessibilidadecortec.R;
 
 public class RestroomFragment extends Fragment {
+
+    TextInputLayout obs1field;
+    TextInputEditText obs1value;
 
     public RestroomFragment() {
         // Required empty public constructor
@@ -30,5 +39,22 @@ public class RestroomFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_restroom, container, false);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        obs1field = view.findViewById(R.id.restroom_accessible_route_obs_field);
+        obs1value = view.findViewById(R.id.restroom_accessible_route_obs_value);
+
+        obs1value.setOnTouchListener((v, event) -> {
+            v.getParent().requestDisallowInterceptTouchEvent(true);
+            if ((event.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_UP) {
+                v.getParent().requestDisallowInterceptTouchEvent(false);
+            }
+            return false;
+        });
     }
 }
