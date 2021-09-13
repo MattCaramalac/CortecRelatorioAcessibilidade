b9d9389bbde0ada0d356d462b6468f10d1139826
MattCaramalac <mattcaramalac@gmail.com>
2021-09-13 14:33:43 -0400

    Gravação e preenchimento de dados nos campos da UpperView

--- app/src/main/java/com/mpms/relatorioacessibilidadecortec/fragments/RestroomUpperViewFragment.java
+++ app/src/main/java/com/mpms/relatorioacessibilidadecortec/fragments/RestroomUpperViewFragment.java
@@ -1,12 +1,6 @@
 package com.mpms.relatorioacessibilidadecortec.fragments;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,22 +8,34 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.bumptech.glide.Glide;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.mpms.relatorioacessibilidadecortec.Dialogs.DialogClass.ExpandImageDialog;
 import com.mpms.relatorioacessibilidadecortec.R;
+import com.mpms.relatorioacessibilidadecortec.entities.RestroomUpViewEntry;
+import com.mpms.relatorioacessibilidadecortec.model.ViewModelEntry;
 
 public class RestroomUpperViewFragment extends Fragment {
 
+    public static final String GATHER_UP_VIEW = "GATHER_UP_VIEW";
+
     ImageButton upperViewImgButton;
     Button saveUpMeasures, returnRestDoorData;
-    Bundle restroomDataBundle;
+    Bundle restroomDataBundle, imgData;
 
-    TextInputLayout measureFieldA, measureFieldB, measureFieldC, measureFieldD, measureFieldE;
-    TextInputEditText measureValueA, measureValueB, measureValueC, measureValueD, measureValueE;
+    TextInputLayout measureFieldA, measureFieldB, measureFieldC, measureFieldD, measureFieldE, upViewObsField;
+    TextInputEditText measureValueA, measureValueB, measureValueC, measureValueD, measureValueE, upViewObsValue;
 
     Double measureA, measureB, measureC, measureD, measureE;
+    String upViewObs;
+
+    ViewModelEntry modelEntry;
 
     public RestroomUpperViewFragment() {
         // Required empty public constructor
@@ -49,13 +55,14 @@ public class RestroomUpperViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        modelEntry = new ViewModelEntry(requireActivity().getApplication());
+        restroomDataBundle = this.getArguments();
         return inflater.inflate(R.layout.fragment_restroom_upper_view, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        restroomDataBundle = this.getArguments();
 
         upperViewImgButton = view.findViewById(R.id.rest_upper_view_image);
 
@@ -64,12 +71,14 @@ public class RestroomUpperViewFragment extends Fragment {
         measureFieldC = view.findViewById(R.id.upper_view_C_measurement_field);
         measureFieldD = view.findViewById(R.id.upper_view_D_measurement_field);
         measureFieldE = view.findViewById(R.id.upper_view_E_measurement_field);
+        upViewObsField = view.findViewById(R.id.upper_view_obs_field);
 
         measureValueA = view.findViewById(R.id.upper_view_A_measurement_value);
         measureValueB = view.findViewById(R.id.upper_view_B_measurement_value);
         measureValueC = view.findViewById(R.id.upper_view_C_measurement_value);
         measureValueD = view.findViewById(R.id.upper_view_D_measurement_value);
         measureValueE = view.findViewById(R.id.upper_view_E_measurement_value);
+        upViewObsValue = view.findViewById(R.id.upper_view_obs_value);
 
         saveUpMeasures = view.findViewById(R.id.save_up_measurements);
         returnRestDoorData = view.findViewById(R.id.return_button);
@@ -77,15 +86,21 @@ public class RestroomUpperViewFragment extends Fragment {
         Glide.with(this).load(R.drawable.upperview).centerCrop().into(upperViewImgButton);
 
         upperViewImgButton.setOnClickListener( v -> {
-            restroomDataBundle.putInt(ExpandImageDialog.IMAGE_ID, R.drawable.upperview);
-            ExpandImageDialog.expandImage(requireActivity().getSupportFragmentManager(), restroomDataBundle);
+            imgData.putInt(ExpandImageDialog.IMAGE_ID, R.drawable.upperview);
+            ExpandImageDialog.expandImage(requireActivity().getSupportFragmentManager(), imgData);
         });
+
+        if (restroomDataBundle.getBoolean(GATHER_UP_VIEW)) {
+            modelEntry.getOneRestroomUpViewEntry(restroomDataBundle.getInt(RestroomFragment.RESTROOM_ID)).observe(getViewLifecycleOwner(), this::gatherUpViewData);
+        }
 
         returnRestDoorData.setOnClickListener( v-> requireActivity().getSupportFragmentManager().popBackStackImmediate());
 
         saveUpMeasures.setOnClickListener( v -> {
             if (checkEmptyMeasurementsFields()) {
-//                inserir dados tabela
+                RestroomUpViewEntry newUpView = newRestUpView(restroomDataBundle);
+                ViewModelEntry.insertRestroomUpViewEntry(newUpView);
+                restroomDataBundle.putBoolean(GATHER_UP_VIEW, true);
                 FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 RestroomSupportBarFragment barFragment = RestroomSupportBarFragment.newInstance();
@@ -127,5 +142,28 @@ public class RestroomUpperViewFragment extends Fragment {
         measureFieldC.setErrorEnabled(false);
         measureFieldD.setErrorEnabled(false);
         measureFieldE.setErrorEnabled(false);
+    }
+
+    public RestroomUpViewEntry newRestUpView(Bundle bundle) {
+        measureA = Double.valueOf(String.valueOf(measureValueA.getText()));
+        measureB = Double.valueOf(String.valueOf(measureValueB.getText()));
+        measureC = Double.valueOf(String.valueOf(measureValueC.getText()));
+        measureD = Double.valueOf(String.valueOf(measureValueD.getText()));
+        measureE = Double.valueOf(String.valueOf(measureValueE.getText()));
+
+        if(!TextUtils.isEmpty(upViewObsValue.getText()))
+        upViewObs = String.valueOf(upViewObsValue.getText());
+
+        return new RestroomUpViewEntry(bundle.getInt(RestroomFragment.RESTROOM_ID), measureA, measureB, measureC,
+                measureD, measureE, upViewObs);
+    }
+
+    public void gatherUpViewData(RestroomUpViewEntry upViewEntry) {
+        measureValueA.setText(String.valueOf(upViewEntry.getUpViewMeasureA()));
+        measureValueB.setText(String.valueOf(upViewEntry.getUpViewMeasureB()));
+        measureValueC.setText(String.valueOf(upViewEntry.getUpViewMeasureC()));
+        measureValueD.setText(String.valueOf(upViewEntry.getUpViewMeasureD()));
+        measureValueE.setText(String.valueOf(upViewEntry.getUpViewMeasureE()));
+        upViewObsValue.setText(upViewEntry.getUpViewObs());
     }
 }
